cfb73f9c0cbbe42414cb4474f195cd0ae40db544
koske <dkoscic@riteh.hr>
2023-02-20 14:33:57 +0100

    ".init.lua" moved to ".init"

--- plugin/.init.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-  nnoremap <silent> <C-k> :lua require("alt-modes.test")()<CR>
-  nnoremap <silent> <leader>K :lua require("alt-modes.core.reload")()<CR>
-]]
-
-vim.notify("Loaded init script")
